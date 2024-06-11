0420b5613422f541ad8f5e4597a430bf87e1fb55
HEADLINE Design <aaron@headline.dev>
2024-06-10 22:36:37 -0500

    update turbo

--- examples/algostack/algostack-app/ui/index.tsx
+++ examples/algostack/algostack-app/ui/index.tsx
@@ -5,4 +5,4 @@ export { Card, CardContent, CardDescription, CardHeader, CardTitle } from './car
 export { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './dropdown-menu';
 export { Input } from './input';
 export {Textarea } from './text-area';
-export { default as Dialog } from './dialog';
+export { default as BaseDialog } from './dialog';
