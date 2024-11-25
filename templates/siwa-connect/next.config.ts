5fd401e949504e2519e4a915ea4aaf3a23206369
HEADLINE Design <aaron@headline.dev>
2024-11-25 02:33:45 -0600

    update config

--- templates/siwa-connect/next.config.ts
+++ templates/siwa-connect/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
